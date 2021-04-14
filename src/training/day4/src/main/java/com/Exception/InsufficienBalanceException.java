cf86c4ea73900f63ca010bdf2dc08419aaf1f1a5
varsha04parihar <pariharvarsha1998@gmail.com>
2021-04-14 17:24:28 +0530

    commit

---
+++ src/training/day4/src/main/java/com/Exception/InsufficienBalanceException.java
@@ -0,0 +1,14 @@
+package com.Exception;
+
+public class InsufficienBalanceException extends Exception
+{
+    public InsufficienBalanceException()
+    {
+        super();
+    }
+    public InsufficienBalanceException(String s)
+    {
+        super(s);
+    }
+}
+
