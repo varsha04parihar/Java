3577e49be9a5dec205c29e144d7d3fd38a5a900f
varsha04parihar <pariharvarsha1998@gmail.com>
2021-04-14 15:15:34 +0530

    commit

---
+++ src/training/day4/src/main/java/InterfaceInClass.java
@@ -0,0 +1,21 @@
+class SomeClass{
+
+    interface SomeInterface{
+        void test1();
+    }
+
+}
+
+class Child2 implements SomeClass.SomeInterface{
+
+
+    public void test1() {
+
+    }
+}
+
+public class InterfaceInClass {
+    public static void main(String[] args) {
+
+    }
+}
