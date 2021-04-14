7bd7e2ccd5c4164faf996ffafe62487e622c2497
varsha04parihar <pariharvarsha1998@gmail.com>
2021-04-14 17:25:10 +0530

    commit

---
+++ src/training/day4/src/main/java/CustomExceptionDemo.java
@@ -0,0 +1,37 @@
+import com.Exception.InsufficienBalanceException;
+class Account
+{
+    int actBal;
+    Account(int actBal)
+    {
+        this.actBal=actBal;
+    }
+    void deposit(int abal)
+    {
+        actBal=actBal+abal;
+        System.out.println("balance after deposit :"+actBal);
+    }
+    void withDraw(int wbal) throws InsufficienBalanceException {
+        if(actBal<wbal)
+        {
+            InsufficienBalanceException ec=new
+                    InsufficienBalanceException();
+            throw ec;
+            //throw new InsufficientBalanceException();
+        }
+    }
+}
+public class CustomExceptionDemo {
+    public static void main(String[] args) {
+        Account a=new Account(50000);
+        try
+        {
+            a.withDraw(700000);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+}
